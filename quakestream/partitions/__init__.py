5cd8804a80ba10cda38c9c4861f293fd4b110aec
Even Wei <shinycockorach@gmail.com>
2024-04-18 02:52:14 +0800

    Chore and update partition
    Signed-off-by: Even Wei <shinycockorach@gmail.com>

--- quakestream/partitions/__init__.py
+++ quakestream/partitions/__init__.py
@@ -2,9 +2,7 @@ from ..assets import constants
 from dagster import MonthlyPartitionsDefinition
 
 start_date = constants.START_DATE
-end_date = constants.END_DATE
 
 monthly_partition = MonthlyPartitionsDefinition(
-    start_date=start_date,
-    end_date=end_date
+    start_date=start_date
 )
