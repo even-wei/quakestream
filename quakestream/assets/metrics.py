787cf9dc3ffa920bd0b819a0f74845b8b3f52126
Even Wei <shinycockorach@gmail.com>
2024-05-01 03:31:18 +0800

    Add stats for the past 30 days and visualization
    Signed-off-by: Even Wei <shinycockorach@gmail.com>

--- quakestream/assets/metrics.py
+++ quakestream/assets/metrics.py
@@ -1,7 +1,39 @@
-from sqlglot import select
+import pickle
+
+from sqlglot import select, condition
 from dagster import asset
 
 from dagster_duckdb import DuckDBResource
+
+@asset(
+    deps=['cwa_tw_earthquakes'],
+    group_name='metrics'
+)
+def earthquakes_by_day_and_magnitude(database: DuckDBResource):
+    """
+      The number of earthquakes from the past 30 days, aggregated by day and magnitude.
+    """
+
+    query = select(
+        'date_trunc(day, origin_time) as day_date',
+        'magnitude',
+        'count(*) as count'
+    ).from_(
+        'earthquakes'
+    ).where(
+        condition("origin_time >= current_date - 30")
+    ).group_by(
+        'day_date',
+        'magnitude'
+    ).order_by(
+        'day_date',
+        'magnitude'
+    ).sql()
+
+    with database.get_connection() as conn:
+        df = conn.execute(query).fetch_df()
+
+    return pickle.dumps(df)
 
 @asset(
     deps=['cwa_tw_earthquakes'],
@@ -23,10 +55,10 @@ def earthquakes_by_month_and_magnitude(database: DuckDBResource):
         'magnitude'
     ).order_by(
         'month_date',
-        'magnitude desc'
+        'magnitude'
     ).sql()
 
     with database.get_connection() as conn:
         df = conn.execute(query).fetch_df()
 
-    return df
+    return pickle.dumps(df)
--- quakestream/assets/notebooks.py
+++ quakestream/assets/notebooks.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 import nbformat
 import jupytext
 from nbconvert.preprocessors import ExecutePreprocessor
@@ -10,13 +9,24 @@ from dagster import asset
 @asset(
     group_name='visualization'
 )
-def monthly_earthquakes_notebook(earthquakes_by_month_and_magnitude):
+def earthquakes_stats_notebook(earthquakes_by_day_and_magnitude, earthquakes_by_month_and_magnitude):
+    """
+    Visualize metrics in notebook
+    """
     markdown = f"""
-# Earthquakes
+# Earthquakes Stats
 
 ```python
 import pickle
-earthquakes_by_month_and_magnitude = pickle.loads({pickle.dumps(earthquakes_by_month_and_magnitude)!r})
+earthquakes_by_day_and_magnitude = pickle.loads({earthquakes_by_day_and_magnitude})
+earthquakes_by_month_and_magnitude = pickle.loads({earthquakes_by_month_and_magnitude})
+```
+
+## Earthquakes by Day and Magnitude
+```python
+import plotly.express as px
+fig = px.bar(earthquakes_by_day_and_magnitude, x='day_date', y='count', color='magnitude')
+fig.show('png')
 ```
 
 ## Earthquakes by Month and Magnitude
@@ -34,13 +44,16 @@ fig.show('png')
 @asset(
     group_name='visualization'
 )
-def monthly_earthquakes_notebook_gist(context, monthly_earthquakes_notebook):
+def earthquakes_stats_notebook_gist(context, earthquakes_stats_notebook):
+    """
+    Upload the notebook to Gits
+    """
     github_api = Github(os.getenv('GITHUB_ACCESS_TOKEN'))
     auth_user = github_api.get_user()
     gist = auth_user.create_gist(
         public=False,
         files={
-            "monthly_earthquakes_notebook.ipynb": InputFileContent(monthly_earthquakes_notebook),
+            "monthly_earthquakes_notebook.ipynb": InputFileContent(earthquakes_stats_notebook),
         },
     )
     context.log.info(f"Notebook created at {gist.html_url}")
